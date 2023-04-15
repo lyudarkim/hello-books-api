85643d06c5c69d76dc53e7100332f251f3869c97
Lyuda Kim <lyudarkim@gmail.com>
2023-04-15 12:23:27 -0700

    Handled 404 and 400 errors, implemented helper function.

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, abort, make_response
 
 
 class Book:
@@ -17,6 +17,23 @@ books = [
 hello_world_bp = Blueprint("hello_world", __name__)
 books_bp = Blueprint("books", __name__, url_prefix="/books")
 
+# Helper function
+def validate_book(book_id):
+    # handle invalid book_id, return 400
+    try:
+        book_id = int(book_id)
+    except:
+        abort(make_response({"message": f"book {book_id} invalid"}, 400))
+    
+    # search for book_id in data, return book
+    for book in books:
+        if book.id == book_id:
+            return book
+    
+    # return a 404 for non-existing book
+    abort(make_response({"message": f"book {book_id} not found"}, 404))
+
+
 @books_bp.route("", methods=["GET"])
 def handle_books():
     books_response = []
@@ -27,6 +44,16 @@ def handle_books():
             "description": book.description,
         })
     return jsonify(books_response), 200
+
+@books_bp.route("/<book_id>", methods=["GET"])
+def handle_book(book_id):
+    book = validate_book(book_id)
+
+    return {
+        "id": book.id,
+        "title": book.title,
+        "description": book.description,
+    }    
 
 @hello_world_bp.route("/hello-world", methods=["GET"])
 def say_hello_world():
