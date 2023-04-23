a50fff3d9f8631fd966b5e98362b3f92bfe0b0ca
Lyuda Kim <lyudarkim@gmail.com>
2023-04-23 13:12:58 -0700

    Refactored to have 2 functions to read all books and create a book.

--- app/routes.py
+++ app/routes.py
@@ -6,8 +6,21 @@ from flask import Blueprint, jsonify, abort, make_response, request
 
 books_bp = Blueprint("books_bp", __name__, url_prefix="/books")
 
+@books_bp.route("",  methods=["GET"])
+def read_all_books():
+    books = Book.query.all()
+    books_response = []
+    for book in books:
+        books_response.append({
+            "id": book.id,
+            "title": book.title,
+            "description": book.description,
+        })
+    return jsonify(books_response), 200
+
+
 @books_bp.route("",  methods=["POST"])
-def handle_books():
+def create_book():
     request_body = request.get_json()
     new_book = Book(title=request_body["title"],
                     description=request_body["description"])
@@ -16,6 +29,8 @@ def handle_books():
     db.session.commit()
 
     return make_response(f"Book {new_book.title} successfully created", 201)
+
+
 
 
 # class Book:
@@ -50,16 +65,6 @@ def handle_books():
 #     abort(make_response({"message": f"book {book_id} not found"}, 404))
 
 
-# @books_bp.route("", methods=["GET"])
-# def handle_books():
-#     books_response = []
-#     for book in books:
-#         books_response.append({
-#             "id": book.id,
-#             "title": book.title,
-#             "description": book.description,
-#         })
-#     return jsonify(books_response), 200
 
 # @books_bp.route("/<book_id>", methods=["GET"])
 # def handle_book(book_id):
