bd19d5b9415bc321b9f2e08f0865648670aa114d
Lyuda Kim <lyudarkim@gmail.com>
2023-04-24 18:17:20 -0700

    Refactored read all books,added update and delete book.

--- app/routes.py
+++ app/routes.py
@@ -6,30 +6,7 @@ from flask import Blueprint, jsonify, abort, make_response, request
 
 books_bp = Blueprint("books_bp", __name__, url_prefix="/books")
 
-@books_bp.route("",  methods=["GET"])
-def read_all_books():
-    books = Book.query.all()
-    books_response = []
-    for book in books:
-        books_response.append({
-            "id": book.id,
-            "title": book.title,
-            "description": book.description,
-        })
-    return jsonify(books_response), 200
-
-@books_bp.route("",  methods=["POST"])
-def create_book():
-    request_body = request.get_json()
-    new_book = Book(title=request_body["title"],
-                    description=request_body["description"])
-    
-    db.session.add(new_book)
-    db.session.commit()
-
-    return make_response(f"Book {new_book.title} successfully created", 201)
-
-# Helper function
+# Helper functions
 def validate_book(book_id):
     # handle invalid book_id, return 400
     try:
@@ -44,6 +21,37 @@ def validate_book(book_id):
     
     return book
 
+# Route functions:
+@books_bp.route("",  methods=["GET"])
+def read_all_books():
+    title_query = request.args.get("title")
+    if title_query:
+        books = Book.query.filter_by(title=title_query)
+    else:
+        books = Book.query.all()
+    
+    books_response = []
+    for book in books:
+        books_response.append({
+            "id": book.id,
+            "title": book.title,
+            "description": book.description,
+        })
+    return jsonify(books_response), 200
+
+
+@books_bp.route("",  methods=["POST"])
+def create_book():
+    request_body = request.get_json()
+    new_book = Book(title=request_body["title"],
+                    description=request_body["description"])
+    
+    db.session.add(new_book)
+    db.session.commit()
+
+    return make_response(f"Book {new_book.title} successfully created", 201)
+
+
 @books_bp.route("/<book_id>", methods=["GET"])
 def read_one_book(book_id):
     book = validate_book(book_id)
@@ -52,6 +60,30 @@ def read_one_book(book_id):
         "title": book.title,
         "description": book.description,
     }  
+
+
+@books_bp.route("/<book_id>", methods=["PUT"])
+def update_book(book_id):
+    book = validate_book(book_id)
+
+    request_body = request.get_json()
+
+    book.title = request_body["title"]
+    book.description = request_body["description"]
+
+    db.session.commit()
+
+    return make_response(f"Book #{book.id} successfully updated")
+
+
+@books_bp.route("/<book_id>", methods=["DELETE"])
+def delete_book(book_id):
+    book = validate_book(book_id)
+
+    db.session.delete(book)
+    db.session.commit()
+
+    return make_response(f"Book #{book.id} successfully deleted")
 
 
 # class Book:
@@ -77,38 +109,3 @@ def read_one_book(book_id):
 #     # return a 404 for non-existing book
 #     abort(make_response({"message": f"book {book_id} not found"}, 404))
 
-
-
-# @books_bp.route("/<book_id>", methods=["GET"])
-# def handle_book(book_id):
-#     book = validate_book(book_id)
-
-#     return {
-#         "id": book.id,
-#         "title": book.title,
-#         "description": book.description,
-#     }    
-
-# @hello_world_bp.route("/hello-world", methods=["GET"])
-# def say_hello_world():
-#     my_beautiful_world = "Hello, World!"
-#     return my_beautiful_world, 200
-
-# @hello_world_bp.route("/hello/JSON", methods=["GET"])
-# def say_hello_json():
-#     return {
-#         "name": "CheezItMan",
-#         "message": "Need more Cheez!",
-#         "hobbies": ["Snacks", "Coding", "Gardening"]
-#     }, 200
-
-# @hello_world_bp.route("/broken-endpoint-with-broken-server-code")
-# def broken_endpoint():
-#     response_body = {
-#         "name": "Ada Lovelace",
-#         "message": "Hello!",
-#         "hobbies": ["Fishing", "Swimming", "Watching Reality Shows"]
-#     }
-#     new_hobby = "Surfing"
-#     response_body["hobbies"] + new_hobby
-#     return response_body
