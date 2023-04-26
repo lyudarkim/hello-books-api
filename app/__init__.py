fb4d4a6052b64c5787336d6dc4bd7bc7f709a321
Lyuda Kim <lyudarkim@gmail.com>
2023-04-25 23:34:28 -0700

    Added environment variables.

--- app/__init__.py
+++ app/__init__.py
@@ -1,23 +1,35 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from dotenv import load_dotenv
+import os
 
 db = SQLAlchemy()
 migrate = Migrate()
+load_dotenv()
 
 
 def create_app(test_config=None):
     app = Flask(__name__)
 
-    # DB Config
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql+psycopg2://postgres:postgres@localhost:5432/hello_books_development"
-
-    # Import models
-    from app.models.book import Book
+    # We are not trying to run the app in a test environment
+    if not test_config:
+        app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+        app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
+            "SQLALCHEMY_DATABASE_URI")
+    # We are trying to test the app
+    else:
+        # Turns testing mode on
+        app.config["TESTING"] = True
+        app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+        app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
+            "SQLALCHEMY_TEST_DATABASE_URI")
 
     db.init_app(app)
     migrate.init_app(app, db)
+
+    # Import models
+    from app.models.book import Book
 
     # Register Blueprints
     from .routes import books_bp
