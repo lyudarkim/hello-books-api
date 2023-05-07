59c9261b8f6c1c567c0f6a35d36ee1fe4f66bc15
Lyuda Kim <lyudarkim@gmail.com>
2023-05-07 13:06:58 -0700

    Added genred attribute to Book model

--- app/models/book.py
+++ app/models/book.py
@@ -1,12 +1,12 @@
 from app import db
 
-
 class Book(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String)
     description = db.Column(db.String)
     author_id = db.Column(db.Integer, db.ForeignKey('author.id'))
     author = db.relationship("Author", back_populates="books")
+    genres = db.relationship("Genre", secondary="book_genre", backref="books")
 
     def to_dict(self):
         book_as_dict = {}
